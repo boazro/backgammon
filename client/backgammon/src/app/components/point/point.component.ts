40e8942a522989aa5ae6ddbf1f8c315c0d1b26ed
boazMac <boaz.rose@gmail.com>
2016-11-14 00:55:01 +0200

    add basic files

--- client/backgammon/src/app/components/point/point.component.ts
+++ client/backgammon/src/app/components/point/point.component.ts
@@ -5,22 +5,15 @@ import { PieceComponent } from '../piece/piece.component';
   selector: 'point',
   templateUrl: './point.component.html',
   styleUrls: ['./point.component.css']
-
 })
 
 export class PointComponent implements OnInit {
   private index: number = 0;
-  //position: string = '';
   private pieces: PieceComponent[];
 
-  constructor(index) {
-     this.pieces = [];
-     this.index = index;
-    //
-    // let verticalPosition = index <= 12 ? 'arrow-up' : 'arrow-down';
-    // let parity = index % 2 == 0 ? 'even' : 'odd';
-    //
-    // this.position = verticalPosition + ' ' + parity;
+  constructor() {
+    this.pieces = [];
+    this.index = index;
   }
 
   ngOnInit() {
